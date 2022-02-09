bc4cf796787b6230af72d16c1e47e2effb2242f1
Mykhailo <m_musienko@outlook.com>
2022-02-09 19:21:06 +0200

    Refactor main method

--- com/mykhailo/vehicles/CarFactory.java
+++ com/mykhailo/vehicles/CarFactory.java
@@ -6,16 +6,14 @@ public class CarFactory {
 
         SportCar sportCar = new SportCar("Nissan","C",1200, 250);
         sportCar.setDriver("Jack Jackson", 20, "20.03.1980");
-        sportCar.setEngine("Renault", 140);
+        sportCar.setEngine("Renault", 180);
         System.out.println(sportCar);
         Lorry lorry = new Lorry("DAF", "F", 5000, 1500);
-        lorry.setDriver("Jack Jackson", 20, "20.03.1982");
+        lorry.setDriver("John Johnson", 20, "15.03.1982");
         lorry.setEngine("Renault", 400);
         System.out.println(lorry);
-        lorry.getDriver().getAge();
-        lorry.start();
-        sportCar.getDriver().getAge();
-        sportCar.start();
+        lorry.driver.getAge();
+        sportCar.driver.getAge();
 
     }
 }
